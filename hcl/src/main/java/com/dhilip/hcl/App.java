4feb6e2ad21165f8e5e218f4ccb73e59fe331152
unknown <dhilip@gmail.com>
2021-04-13 20:21:08 +0530

    first commit

---
+++ hcl/src/main/java/com/dhilip/hcl/App.java
@@ -0,0 +1,16 @@
+package com.dhilip.hcl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        System.out.println( "Hello EveryOne!" );
+        System.out.println( "Hello!" );
+    }
+}
